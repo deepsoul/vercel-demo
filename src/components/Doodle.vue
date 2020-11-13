fa04cc4bf584acf3710ec0ccb1b7edc5a25a0bc8
Boris Horn <horn.boris@gmail.com>
2020-11-13 14:49:48 +0100

    add firebase

--- src/components/Doodle.vue
+++ src/components/Doodle.vue
@@ -1,13 +1,121 @@
+
 <template>
-  <h1>This is the Doodle</h1>
+  <div>
+    <h1 class="headline">Cookies Doodle</h1>
+    <div class="counter">
+        <Counter v-if="bCount > 0" :count="bCount" :name="'Boris'" />
+        <Counter v-if="jCount > 0" :count="jCount" :name="'Jacky'" />
+    </div>
+    <div class="button">
+    <button @click="onAddDoodle('boris')">Vote Boris</button>
+    <button @click="onAddDoodle('jacky')">Vote Jacky</button>
+    </div>
+    
+  
+  </div>
 </template>
 
 <script>
+import Counter from './atoms/Counter';
 export default {
+    components : {
+        Counter
+    },
+  data() {
+    return {
+      testCollection: [],
+      bCount : 0,
+      jCount: 0
+    };
+  },
+  methods: {
+      onAddDoodle(user) {
+            const db = this.$firebase.firestore();
+             let doodle = {}
+            if(user === 'boris') {
+                doodle = {boris: 1, jacky: 0}
+            }else {
+                doodle = {boris: 0, jacky: 1}
+            }
 
-}
+            db
+            .collection('cookies')
+            .add(doodle).then(() => {
+                    // eslint-disable-next-line no-console
+                    console.log("allright")
+                }).catch((error) => {
+                    // eslint-disable-next-line no-console
+                    console.log(error);
+                });
+      }
+  },
+  created() {
+  const db = this.$firebase.firestore();
+            db
+            .collection('cookies')
+            .onSnapshot((snapshotChange) => {
+               
+                let bCount = 0;
+                let jCount = 0;
+                snapshotChange.forEach((doc) => {
+                    bCount += doc.data().boris;
+                    jCount += doc.data().jacky;
+                });
+
+                this.bCount = bCount;
+                this.jCount = jCount;
+
+            })
+        },
+
+  mounted() {
+    const db = this.$firebase.firestore();
+    db
+      .collection('cookies')
+      .get()
+      .then(snap => {
+        let bCount = 0;
+        let jCount = 0;
+        snap.forEach(doc => {
+          bCount += doc.data().boris;
+          jCount += doc.data().jacky;
+         
+        });
+
+        this.bCount = bCount;
+                this.jCount = jCount;
+  
+      });
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+h1.headline {
+    font-size: 64px;
+    margin-top: 16px;
+    color: #206fb7;
+}
+.counter {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+   justify-content: center;
 
+   .card {
+       margin:1em;
+   }
+}
+
+.button {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+   justify-content: center;
+
+   button {
+       margin: 1em;
+   }
+
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import firebase from 'firebase';
+require('firebase/firestore');
+
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBknbPlYzWsKlU_DrcP9LMTqLd-vLU9hQ0",
+    authDomain: "nfh-doodle.firebaseapp.com",
+    databaseURL: "https://nfh-doodle.firebaseio.com",
+    projectId: "nfh-doodle",
+    storageBucket: "nfh-doodle.appspot.com",
+    messagingSenderId: "667643233011",
+    appId: "1:667643233011:web:9a7728b98b4687385bed86"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  /* Bind firebase to your Vue instance */
+Vue.prototype.$firebase = firebase;
+
+
 Vue.config.productionTip = false
 
 new Vue({
